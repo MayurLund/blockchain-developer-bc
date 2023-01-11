5d328fc7ac71dcf2ecdd3c58516864ef50e36f44
MayurLund <lundmayur07@gmail.com>
2023-01-12 03:27:38 +0530

    2. Smoke Test

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+contract Token {
+    string public name = "My Token";
+    //state variable belongs to smart contracts and that value gets directly written to blockchain
+    //first in contract and after deployed it fetches from blockchain
+}
